41dca119f36a62186d94cae540fcc6b1ef4d8893
Juan <juanandresroth2@gmail.com>
2026-02-12 20:11:25 -0300

    modificación de funcion 'css' para sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ import gulpSass from 'gulp-sass'
 const sass = gulpSass(dartSass)
 
 export function css(done) {
-    src('src/scss/**/*.scss') // ruta completa del arvhivo
+    src('src/scss/**/*.scss', {sourcemaps: true}) // ruta completa del arvhivo
         .pipe( sass() .on('error', sass.logError) ) 
-        .pipe( dest('build/css') )
+        .pipe( dest('build/css', {sourcemaps: true}) )
         
     done()
 }
