d235835fc908dd91227adad85c7ca786b27cec07
Juan <juanandresroth2@gmail.com>
2026-02-14 14:17:18 -0300

    agregando imagenes a seccion de galería con JS

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,17 @@
-console.log('Alerta!');
+document.addEventListener('DOMContentLoaded', function() {
+    crarGalería()
+})
+
+function crarGalería () {
+
+    const ultimaImagen = 16
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for (let i = 1; i <= ultimaImagen ; i++) { //Nos permite crear la variable 'i' que coincide con el nombre de las img
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg` //Agrega el atributo de la ubicación de la imagen.
+        imagen.alt = 'Imagen Galería' //Generamos el texto alternativo.
+        
+        galeria.appendChild(imagen)
+    }
+}
