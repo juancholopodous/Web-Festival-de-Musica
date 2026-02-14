32f6d41af3222ec38509588fbb22066230bd7b42
Juan <juanandresroth2@gmail.com>
2026-02-14 19:52:46 -0300

    configuración de latencia en cierre de ventana Modal y cración de clase fade-out mediante JavaScript

--- build/js/app.js
+++ build/js/app.js
@@ -11,7 +11,38 @@ function crarGalería () {
         const imagen = document.createElement('IMG')
         imagen.src = `src/img/gallery/full/${i}.jpg` //Agrega el atributo de la ubicación de la imagen.
         imagen.alt = 'Imagen Galería' //Generamos el texto alternativo.
-        
+
+        // Event Handler
+        imagen.onclick = function () {
+            mostrarImagen(i)
+        } 
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen (i) {
+    const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galería'
+
+    // Generar Modal
+    const modal = document.createElement('DIV') // lo usamos para oscurecer el fondo.
+    modal.classList.add('modal') //Generamos la clase para darle estilos con SASS
+    modal.onclick = cerrarModal //Cierra el modal al hacer Click.
+
+    modal.appendChild(imagen) // Agrega imagen al HTML
+
+    // Agregar al HTML
+    const body = document.querySelector('body')
+    body.appendChild(modal)
+}
+
+function cerrarModal () {
+    const modal = document.querySelector('.modal') //Seleccionamos la clase con el '.'
+    modal.classList.add('fade-out')
+
+    //Retrasamos el cierre para que funcione la transición.
+    setTimeout(() => {
+        modal?.remove() // la lógica es: Si existe modal? Entonces elimínalo
+    }, 500); //cada 1000 es un segundo.
 }
